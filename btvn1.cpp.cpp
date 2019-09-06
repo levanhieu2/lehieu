e7b1bc04c38349ef1c62fc84d17fb12f67b2fc9a
levanhieu2 <54978180+levanhieu2@users.noreply.github.com>
2019-09-06 17:20:30 +0700

    Create btvn1.cpp.cpp

---
+++ btvn1.cpp.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+   int main (){
+	int n;
+	int i=1;
+	printf (" in ra cac so le be hon n:");
+	scanf ("%d",&n);
+	printf (" cac so le be hon n:\n");
+	
+	while (i<n){
+	if(i%2==0)	;
+	else printf ("%d \n",i);	
+	i++;
+	}
+
+	
+	
+	
+	return 0;
+	
+}
