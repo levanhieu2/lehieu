9261f04a152f3e82e19c355b5fe50d2f5bb90c12
levanhieu2 <54978180+levanhieu2@users.noreply.github.com>
2019-09-09 21:53:53 +0700

    Create btvn2.step7.cpp.cpp

---
+++ btvn2.step7.cpp.cpp
@@ -0,0 +1,13 @@
+   #include <stdio.h>
+   int main (){
+   	int a[20];
+   	  for (int i = 0;i < 20; i++){
+   	  	scanf ("%d",&a[i]);
+   	  }
+   	     for(int x,i = 0;i < 20; ++i){
+   	     	if (a[i]%2!=0){
+   	     		printf (" la so le: %d\n",a[i]);
+   	     	}
+				}
+				return 0;
+			}
