01e8b1ff03f3a6556763725b0ce3d59b8ed15529
levanhieu2 <54978180+levanhieu2@users.noreply.github.com>
2019-09-09 21:53:50 +0700

    Create btvn3.step7.cpp.cpp

---
+++ btvn3.step7.cpp.cpp
@@ -0,0 +1,14 @@
+	   #include <stdio.h>
+   int main (){
+   	int n=6;
+   	int i;
+   	for ( int i=1; i<=n; i++){
+	   
+	   for ( int b =1; b <= i; b++){
+	   	printf ("*",n);
+}
+	   printf ("\n");
+	   
+}
+return 0;
+}
